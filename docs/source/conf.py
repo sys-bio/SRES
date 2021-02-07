d58db5cfd7dc1d45f08512219701c4f34459e5b3
Ciaran Welsh <cwelsh2@uw.edu>
2021-02-07 19:14:22 +0000

    increment version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Ciaran Welsh'
 author = 'Ciaran Welsh'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
